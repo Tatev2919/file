dc0f7d696313c09a2780db8e02ee433bda6ae641
Tatev2919 <66516027+Tatev2919@users.noreply.github.com>
2021-02-04 13:13:21 +0400

    Add files via upload

---
+++ file.py
@@ -0,0 +1,38 @@
+import random
+import sys
+file  = open("test.txt","w+")
+file.write("What is the longest river in Russia and Europe ? _ Volga \n")
+file.write("What is the capital city of Armenia ? _ Yerevan \n")
+file.write("5+5 ? _ 10 \n")
+file.write("15*2= ? _ 30 \n")
+x = random.randint(1,4);
+file.close()
+count = 0
+question = ""
+answer = ""
+f = open("test.txt","r")
+f1 = f.readlines()
+for line in f1:
+    count +=1
+    if(count == x):
+        question = line[:line.index('_')]
+        answer = line[line.index('_')+1:].strip()
+
+print(question)
+str = '-'*len(answer)
+print(str)
+flag = False
+for line in sys.stdin:
+    for var in line.split():
+        for i in range(len(var)):
+            for k in range(len(answer)):
+                if(var[i] == answer[k]):
+                    str = str[:k]+var[i]+str[k+1:];
+        print(str);
+        if (var == answer or str == answer):
+            print("Congrats!")
+            flag = True
+
+    if(flag):
+        break
+file.close()

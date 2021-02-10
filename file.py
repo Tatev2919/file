8b22d8957420fcf1efbeaf1bc12f75ff1e10cb80
Tatev2919 <66516027+Tatev2919@users.noreply.github.com>
2021-02-10 15:22:45 +0400

    Update file.py

--- file.py
+++ file.py
@@ -1,38 +1,37 @@
 import random
 import sys
-file  = open("test.txt","w+")
+
+file = open("test.txt", "w+")
 file.write("What is the longest river in Russia and Europe ?  Volga \n")
 file.write("What is the capital city of Armenia ?  Yerevan \n")
 file.write("5+5 ? 10 \n")
 file.write("15*2= ?  30 \n")
-x = random.randint(1,4);
+x = random.randint(1, 4);
 file.close()
 count = 0
 question = ""
 answer = ""
-f = open("test.txt","r")
+f = open("test.txt", "r")
 f1 = f.readlines()
 for line in f1:
-    count +=1
-    if(count == x):
+    count += 1
+    if (count == x):
         question = line[:line.index('?')]
-        answer = line[line.index('?')+1:].strip()
+        answer = line[line.index('?') + 1:].strip()
 
 print(question)
-str = '-'*len(answer)
+str = '-' * len(answer)
 print(str)
-flag = False
-for line in sys.stdin:
-    for var in line.split():
-        for i in range(len(var)):
-            for k in range(len(answer)):
-                if(var[i] == answer[k]):
-                    str = str[:k]+var[i]+str[k+1:];
-        print(str);
-        if (var == answer or str == answer):
-            print("Congrats!")
-            flag = True
 
-    if(flag):
-        break
+#if we enter only 1 char
+
+while str != answer:
+    var = input("enter answer")
+    if(len(var) > 1):
+        var = input("Reenter answer")
+    for j in range(len(answer)):
+        if (var == answer[j]):
+            str = str[:j] + var + str[j + 1:]
+    print(str)
+print("Congrats!")
 file.close()
